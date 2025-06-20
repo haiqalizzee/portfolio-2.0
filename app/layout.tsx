f6d916f76ec0320326c3c9bca1bdcf68b74a5a79
haicoool <haicoool@users.noreply.github.com>
2025-06-21 00:15:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ const robotoFlex = Roboto_Flex({
 });
 
 export const metadata: Metadata = {
-    title: 'Portfolio - Haiqal Izzee',
+    title: 'Haiqal Izzee Portfolio',
     description: 'Personal portfolio of Haiqal Izzee',
 };
 
